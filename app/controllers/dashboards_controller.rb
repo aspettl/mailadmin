ce7c7435e175b0868d9e7d5efccaaa39646499e2
Aaron Spettl <aaron@spettl.de>
2021-05-18 10:00:08 +0200

    Add some numbers to dashboard page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,5 +5,7 @@ class DashboardsController < ApplicationController
   before_action :add_dashboard_breadcrumb
 
   def show
+    @domains_count = @user.domains.count
+    @accounts_count = @user.accounts.count
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :validatable, :timeoutable, :lockable
   has_many :domains, dependent: :destroy
+  has_many :accounts, through: :domains
 end
