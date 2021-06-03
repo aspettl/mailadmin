161a767b80f9463eee4fa23ecfbc029bfe730997
Aaron Spettl <aaron@spettl.de>
2021-06-03 19:21:47 +0200

    Always allow to re-set existing password for bcrypt

--- app/models/account.rb
+++ app/models/account.rb
@@ -16,7 +16,7 @@ class Account < ApplicationRecord
     record.errors.add(attr, 'must be an account under the currently selected domain name') unless value.ends_with?("@#{record.domain.domain}")
   end
   validates :enabled, inclusion: { in: [ true, false ] }
-  validates :password, presence: { on: :create, unless: :crypt? }, length: { minimum: 10, allow_blank: true }, if: :local_mailbox?
+  validates :password, presence: { on: :create, unless: :crypt? }, length: { minimum: 10, allow_blank: true, unless: :password_unchanged? }, if: :local_mailbox?
   validates :password, inclusion: { in: [ nil, '' ], message: 'must be empty when this is an alias address' }, unless: :local_mailbox?
   validates :forward, inclusion: { in: [ true, false ] }
   validates :forward_to, presence: { if: :forward }, format: { with: EMAIL_REGEXP, allow_blank: true }, length: { maximum: 255 }, if: :local_mailbox?
@@ -54,6 +54,10 @@ class Account < ApplicationRecord
   def matches_crypted_password?(password)
     return false if password.blank? or self.crypt.blank?
     password.crypt(self.crypt) == self.crypt
+  end
+
+  def password_unchanged?
+    self.matches_crypted_password?(self.password)
   end
 
   private
