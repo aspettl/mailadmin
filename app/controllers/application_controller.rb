a3115000f4ce5b885996620a393d9dff1abba28e
Aaron Spettl <aaron@spettl.de>
2022-08-19 11:22:56 +0200

    Mark parameter as unused for rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     add_breadcrumb 'Manage domains', :domains_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
