013e75f1238fed0da6c0b649a55b27d1cd128cda
Aaron Spettl <aaron@spettl.de>
2023-10-07 10:33:49 +0200

    Switch to jinjanator for python 3.12 compatibility

--- integrations/docker-mailserver-configreload/bin/build_config.sh
+++ integrations/docker-mailserver-configreload/bin/build_config.sh
@@ -7,13 +7,13 @@ then
   exit 1
 fi
 
-if ! which j2 >/dev/null
+if ! which jinjanate >/dev/null
 then
-  echo "Make sure you installed j2: pip install j2cli"
+  echo "Make sure you installed jinjanate: pip install jinjanator"
   exit 1
 fi
 
 JSON=$(curl --silent --fail --header "Authorization: Bearer $MAILADMIN_API_TOKEN" --write-out "%{stderr}[$(date)] Getting JSON data, HTTP status code: %{http_code}\n%{stdout}\n" $MAILADMIN_URL/api/v1/export.json)
 
-echo "$JSON" | j2 --format=json /usr/local/share/templates/postfix-accounts.cf > $CONFIG_TMP/postfix-accounts.cf
-echo "$JSON" | j2 --format=json /usr/local/share/templates/postfix-virtual.cf > $CONFIG_TMP/postfix-virtual.cf
+echo "$JSON" | jinjanate --format=json /usr/local/share/templates/postfix-accounts.cf > $CONFIG_TMP/postfix-accounts.cf
+echo "$JSON" | jinjanate --format=json /usr/local/share/templates/postfix-virtual.cf > $CONFIG_TMP/postfix-virtual.cf
